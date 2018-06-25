b20cfedbdd27997cfc5a95bc644396aabf565b0d
Brendan Kellams <brendan.kellams@gmail.com>
2018-06-25 08:32:50 -0700

    making changes

---
+++ server.js
@@ -0,0 +1,17 @@
+var path = require("path");
+var express = require("express");
+
+var DIST_DIR = path.join(__dirname, "dist");
+var PORT = 3000;
+var app = express();
+
+// Serving files on the dist folder
+app.use(express.static(DIST_DIR));
+
+// Send index.html when the user acess the web
+app.get("*", function (req, res) {
+  res.sendFile(path.join(DIST_DIR, "index.html"));
+});
+
+
+app.listen(PORT);
---
+++ src/main.js
@@ -0,0 +1,5 @@
+require("file?name-index.html!./index.html");
+
+const hello = document.getElementById("hello");
+
+hello.innerHTML = "Hello World!"; 
