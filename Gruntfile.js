6d5085bb74143449796a1e78221342941da138fa
Brendan Kellams <brendan.kellams@gmail.com>
2018-07-10 15:10:19 -0700

    adding grunt and nodemon

---
+++ Gruntfile.js
@@ -0,0 +1,28 @@
+module.exports = function(grunt) {
+
+  // Project configuration.
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    sass: {
+      dist: {
+        files: {
+          './public/stylesheets/main.css': './public/scss/main.scss',
+        }
+      }
+    },
+    watch: {
+      scss: {
+        files: ['**/*.scss'],
+        tasks: ['sass']  
+      }
+    }
+  });
+
+  // Load the plugin that provides the "uglify" task.
+  grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+
+  // Default task(s).
+  grunt.registerTask('default', ['watch']);
+
+};
