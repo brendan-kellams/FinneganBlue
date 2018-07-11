4a1bff8e50610445bb32da378b0ffe216d7bc05d
Brendan Kellams <brendan.kellams@gmail.com>
2018-07-10 18:45:52 -0700

    adding a temp merch page

--- routes/index.js
+++ routes/index.js
@@ -2,38 +2,38 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', function (req, res, next) {
   res.render('index', { title: 'Finnegan Blue' });
 });
 
 /* GET bio page. */
-router.get('/bio', function(req, res, next) {
+router.get('/bio', function (req, res, next) {
   res.render('bio', { title: 'Finnegan Blue' });
 });
 
 /* GET music page. */
-router.get('/music', function(req, res, next) {
+router.get('/music', function (req, res, next) {
   res.render('music', { title: 'Finnegan Blue' });
 });
 
 /* GET photos page. */
-router.get('/photos', function(req, res, next) {
+router.get('/photos', function (req, res, next) {
   res.render('photos', { title: 'Finnegan Blue' });
 });
 
 /* GET videos page. */
-router.get('/videos', function(req, res, next) {
+router.get('/videos', function (req, res, next) {
   res.render('videos', { title: 'Finnegan Blue' });
 });
 
 /* GET shows page. */
-router.get('/shows', function(req, res, next) {
+router.get('/shows', function (req, res, next) {
   res.render('shows', { title: 'Finnegan Blue' });
 });
 
-/* GET error page. */
-router.get('/bio', function(req, res, next) {
-  res.render('bio', { title: 'Finnegan Blue' });
+/* GET merch page. */
+router.get('/merch', function (req, res, next) {
+  res.render('merch', { title: 'Finnegan Blue' });
 });
 
 
